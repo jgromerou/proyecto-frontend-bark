778b1d0cbb00e5c116975be9b2a9764c8b6c804a
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-04 15:00:38 -0300

    se agregó la estructura básica del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Container className="paginaPrincipal my-5">
+        <h1>Iniciando el proyecto de mini red social Bark</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
